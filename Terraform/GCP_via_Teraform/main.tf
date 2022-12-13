3f2b4d35c39e716691d66af96f56c708de89ffe6
Lloyd Zedds <lloydzedds@gmail.com>
2022-12-13 08:47:54 -0800

    gcp sleep module

--- Terraform/GCP_via_Teraform/main.tf
+++ Terraform/GCP_via_Teraform/main.tf
@@ -11,6 +11,38 @@ resource "google_compute_network" "VPC" {
     auto_create_subnetworks = true
   
 }
+resource "google_compute_firewall" "practice-vpc" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "22", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "google_compute_network" "default" {
+  name = "test-network"
+}
 resource "google_compute_instance" "vminstance" {
   name = "vminstance"
   machine_type = "e2-medium"
