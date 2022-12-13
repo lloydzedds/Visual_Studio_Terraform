55b6b7bca9253f672d8255edacd4437a3b7d27c9
Lloyd Zedds <lloydzedds@gmail.com>
2022-12-13 13:29:05 -0800

    yes

--- Terraform/terraform variables/variables.tf
+++ Terraform/terraform variables/variables.tf
@@ -5,3 +5,9 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
 default = "eu-north-1"
 }
+
+variable "public_key_path" {
+    description = "Public key path"
+    default = "./gcp_ansible.pub"
+  
+}
